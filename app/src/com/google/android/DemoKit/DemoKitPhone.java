1598fc40f281b221586c35b6b658bdf1e462cc29
follower <none@none>
2011-05-15 22:46:30 +1200

    Remove the two tab interface & related resources.

--- app/src/com/google/android/DemoKit/DemoKitPhone.java
+++ app/src/com/google/android/DemoKit/DemoKitPhone.java
@@ -7,11 +7,9 @@ import android.view.View.OnClickListener;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
-public class DemoKitPhone extends BaseActivity implements OnClickListener {
+public class DemoKitPhone extends BaseActivity {
 	static final String TAG = "DemoKitPhone";
 	/** Called when the activity is first created. */
-	TextView mInputLabel;
-	TextView mOutputLabel;
 	LinearLayout mInputContainer;
 	LinearLayout mOutputContainer;
 	Drawable mFocusedTabImage;
@@ -24,54 +22,14 @@ public class DemoKitPhone extends BaseActivity implements OnClickListener {
 		mOutputController = null;
 	}
 
-	public void onCreate(Bundle savedInstanceState) {
-		mFocusedTabImage = getResources().getDrawable(
-				R.drawable.tab_focused_holo_dark);
-		mNormalTabImage = getResources().getDrawable(
-				R.drawable.tab_normal_holo_dark);
-		super.onCreate(savedInstanceState);
-	}
-
 	protected void showControls() {
 		super.showControls();
 
 		mOutputController = new OutputController(this, false);
 		mOutputController.accessoryAttached();
-		mInputLabel = (TextView) findViewById(R.id.inputLabel);
-		mOutputLabel = (TextView) findViewById(R.id.outputLabel);
 		mInputContainer = (LinearLayout) findViewById(R.id.inputContainer);
-		mOutputContainer = (LinearLayout) findViewById(R.id.outputContainer);
-		mInputLabel.setOnClickListener(this);
-		mOutputLabel.setOnClickListener(this);
 
-		showTabContents(true);
-	}
-
-	void showTabContents(Boolean showInput) {
-		if (showInput) {
-			mInputContainer.setVisibility(View.VISIBLE);
-			mInputLabel.setBackgroundDrawable(mFocusedTabImage);
-			mOutputContainer.setVisibility(View.GONE);
-			mOutputLabel.setBackgroundDrawable(mNormalTabImage);
-		} else {
-			mInputContainer.setVisibility(View.GONE);
-			mInputLabel.setBackgroundDrawable(mNormalTabImage);
-			mOutputContainer.setVisibility(View.VISIBLE);
-			mOutputLabel.setBackgroundDrawable(mFocusedTabImage);
-		}
-	}
-
-	public void onClick(View v) {
-		int vId = v.getId();
-		switch (vId) {
-		case R.id.inputLabel:
-			showTabContents(true);
-			break;
-
-		case R.id.outputLabel:
-			showTabContents(false);
-			break;
-		}
+		mInputContainer.setVisibility(View.VISIBLE);		
 	}
 
 }
