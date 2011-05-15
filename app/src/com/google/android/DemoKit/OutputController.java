e4bef405e870c6d22f4ac05b937a35798753595d
follower <none@none>
2011-05-15 17:19:37 +1200

    Remove LED control related resources and code.

--- app/src/com/google/android/DemoKit/OutputController.java
+++ app/src/com/google/android/DemoKit/OutputController.java
@@ -16,10 +16,6 @@ public class OutputController extends AccessoryController {
 		setupServoController(2, R.id.servo2);
 		setupServoController(3, R.id.servo3);
 
-		setupLedController(1, R.id.leds1);
-		setupLedController(2, R.id.leds2);
-		setupLedController(3, R.id.leds3);
-
 		setupRelayController(1, R.id.relay1);
 		setupRelayController(2, R.id.relay2);
 	}
@@ -27,12 +23,6 @@ public class OutputController extends AccessoryController {
 	private void setupServoController(int servoIndex, int viewId) {
 		ServoController sc = new ServoController(mHostActivity, servoIndex);
 		sc.attachToView((ViewGroup) findViewById(viewId));
-	}
-
-	private void setupLedController(int index, int viewId) {
-		ColorLEDController ledC = new ColorLEDController(mHostActivity, index,
-				getResources(), mVertical);
-		ledC.attachToView((ViewGroup) findViewById(viewId));
 	}
 
 	private void setupRelayController(int index, int viewId) {
