246d175405fcfbb3eae5e87792a2673b3c2a1053
follower <follower@rancidbacon.com>
2011-06-12 23:32:52 +1200

    Because we don't make use of the example app's dual Phone/Tablet UI approach we can remove the multiple layers of indirection used to start the app. (While this functionality is in theory useful it really complicates the app and I think simple is better for a starter/learner app.)

--- app/src/com/rancidbacon/Handbag/UsbAccessoryActivity.java
+++ app/src/com/rancidbacon/Handbag/UsbAccessoryActivity.java
@@ -33,7 +33,7 @@ public final class UsbAccessoryActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 
-		Intent intent = HandbagLaunch.createIntent(this);
+		Intent intent = new Intent(this, BaseActivity.class);
 		intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK
 				| Intent.FLAG_ACTIVITY_CLEAR_TOP);
 		try {
