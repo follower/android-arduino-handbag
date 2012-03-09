73bbb72740a40f170afcc485dd4137de888e6a51
follower <follower@rancidbacon.com>
2012-03-10 05:30:04 +1300

    Add initial generated files for a new project. Add README for project which is to experiment with a re-architectured version of the Handbag application.

---
+++ app2/README.txt
@@ -0,0 +1,2 @@
+This code is for experimenting with a re-architected version of the Handbag app
+to split the UI/Events handling from the messaging parsing and data transfer.
---
+++ app2/src/com/handbagdevices/handbag/HandbagUI.java
@@ -0,0 +1,13 @@
+package com.handbagdevices.handbag;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class HandbagUI extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
