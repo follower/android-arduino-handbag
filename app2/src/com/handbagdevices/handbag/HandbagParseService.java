82833d58aca4986a04151bcef3f770e50fa7224c
follower <follower@rancidbacon.com>
2012-03-12 02:04:34 +1300

    Initial hacky test of having a UI activity and a service to feed it (what will become the Parse Service). Currently the service sends a test message every 5-10 seconds which the UI displays a dialog for.

---
+++ app2/src/com/handbagdevices/handbag/HandbagParseService.java
@@ -0,0 +1,111 @@
+package com.handbagdevices.handbag;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.Handler;
+import android.os.IBinder;
+import android.os.Message;
+import android.os.Messenger;
+import android.os.RemoteException;
+import android.util.Log;
+
+public class HandbagParseService extends Service {
+	
+	// Messages from UI activity
+	static final int MSG_UI_ACTIVITY_REGISTER = 101;
+	static final int MSG_UI_SHUTDOWN_REQUEST = 102;
+	
+	static final int MSG_UI_EVENT_OCCURRED = 100;
+	
+	// Messages from communications service
+	static final int MSG_COMM_SERVICE_REGISTER = 200;	
+	
+	// Used to communicate with the UI Activity & Communication Service 
+	Messenger uiActivity = null;
+	Messenger commService = null; // TODO: Implement service.	
+	
+	// Flag that should be checked
+	private boolean shutdownRequested = false;
+	
+	private Handler testMessageHandler = new Handler() {
+
+		@Override
+		public void handleMessage(Message msg) {
+
+			if (!shutdownRequested) {
+				// Only continue if we haven't been told to shutdown.
+
+				try {
+					uiActivity.send(Message.obtain(null, HandbagUI.MSG_UI_TEST_MESSAGE));
+				} catch (RemoteException e) {
+					// UI Activity client is dead so no longer try to access it.
+					uiActivity = null;
+				}
+
+				testMessageHandler.sendEmptyMessageDelayed(0, 10000);				
+			}
+	
+		}
+	
+	};
+	
+	private void startTestMessages() {
+		testMessageHandler.removeCallbacksAndMessages(null);
+		testMessageHandler.sendEmptyMessageDelayed(0, 5000);
+	}
+	
+	// Used to receive messages from client(s)
+	public class IncomingParseServiceHandler extends Handler {
+
+		@Override
+		public void handleMessage(Message msg) {
+			Log.d(this.getClass().getSimpleName(), "Parse Service received message:");
+			
+			switch (msg.what) {
+				case MSG_UI_ACTIVITY_REGISTER:
+					// TODO: Handle receiving this more than once?
+					Log.d(this.getClass().getSimpleName(), "    MSG_UI_ACTIVITY_REGISTER");
+					uiActivity = msg.replyTo;
+					
+					shutdownRequested = false;
+					
+					try {
+						uiActivity.send(Message.obtain(null, HandbagUI.MSG_UI_ACTIVITY_REGISTERED));
+					} catch (RemoteException e) {
+						// UI Activity client is dead so no longer try to access it.
+						uiActivity = null;
+					}
+					
+					if (uiActivity != null) {
+						startTestMessages();
+					}
+					
+					break;
+					
+				case MSG_UI_SHUTDOWN_REQUEST:
+					Log.d(this.getClass().getSimpleName(), "    MSG_UI_SHUTDOWN_REQUEST");
+					
+					// Set a flag so repeating sub-tasks will stop themselves.
+					shutdownRequested = true;
+					// TODO: Be more proactive and stop them here instead?
+					break;
+				
+				default:
+					Log.d(this.getClass().getSimpleName(), "    (unknown)");					
+					super.handleMessage(msg);					
+			}
+		}
+		
+	}
+
+	// Clients will use this to communicate with us.
+    final Messenger ourMessenger = new Messenger(new IncomingParseServiceHandler());
+	
+	@Override
+	public IBinder onBind(Intent intent) {
+		// Return our `messenger` instance to enable clients to send us messages.
+		Log.d(this.getClass().getSimpleName(), "onBind entered");
+		return ourMessenger.getBinder();
+	}
+
+}
--- app2/src/com/handbagdevices/handbag/HandbagUI.java
+++ app2/src/com/handbagdevices/handbag/HandbagUI.java
@@ -1,13 +1,140 @@
 package com.handbagdevices.handbag;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.ServiceConnection;
 import android.os.Bundle;
+import android.os.IBinder;
+import android.os.Message;
+import android.os.Messenger;
+import android.os.RemoteException;
+import android.util.Log;
+import android.os.Handler;
 
 public class HandbagUI extends Activity {
+	
+	// Messages to UI activity (i.e. us)
+	static final int MSG_UI_ACTIVITY_REGISTERED = 1;
+	static final int MSG_UI_TEST_MESSAGE = 2;
+	
+	
+	Messenger parseService = null;
+	boolean parseServiceIsBound = false;
+	
+	// Used to receive messages from service(s)
+	class IncomingUiHandler extends Handler {
+		@Override
+		public void handleMessage(Message msg) {
+			switch (msg.what) {
+				case MSG_UI_ACTIVITY_REGISTERED:
+					Log.d(this.getClass().getSimpleName(), "received: MSG_UI_ACTIVITY_REGISTERED");
+					break;
+					
+				case MSG_UI_TEST_MESSAGE:
+					new AlertDialog.Builder(HandbagUI.this).setMessage("Message received!").show();
+					break;
+				
+				default:
+					super.handleMessage(msg);
+			}
+		}
+	}
+	
+	// Services we connect to will use this to communicate with us.	
+    final Messenger ourMessenger = new Messenger(new IncomingUiHandler());
+    
+	
+	private ServiceConnection connParseService = new ServiceConnection() {
+
+		public void onServiceConnected(ComponentName className, IBinder service) {
+			// Store the object we will use to communicate with the service.
+			parseService = new Messenger(service);
+			parseServiceIsBound = true;
+			Log.d(this.getClass().getSimpleName(), "Parse Service bound");
+			
+			registerWithParseServer();			
+		}
+
+		public void onServiceDisconnected(ComponentName className) {
+			// Called when the service crashes/unexpectedly disconnects.
+			parseService = null;
+			parseServiceIsBound = false;
+		}
+		
+	};
+	
+	
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
     }
+
+    
+    private void registerWithParseServer() {
+    	
+    	Log.d(this.getClass().getSimpleName(), "Registering with Parse Server.");
+    	
+		Message msg = Message.obtain(null, HandbagParseService.MSG_UI_ACTIVITY_REGISTER);
+		msg.replyTo = ourMessenger;					
+		
+		try {
+			parseService.send(msg);
+		} catch (RemoteException ex1) {
+			// Service crashed so just ignore it
+			// TODO: Do something else?
+		}
+    	
+    }
+    
+
+	@Override
+	protected void onStart() {
+		Log.d(this.getClass().getSimpleName(), "Entered onStart()");		
+		super.onStart();
+		
+		// Bind to Parse Service which receives configuration information from the data
+		// source, and to which we send event information. TODO: Improve class name?
+		boolean bindSuccessful = bindService(new Intent(HandbagUI.this, HandbagParseService.class), connParseService, Context.BIND_AUTO_CREATE);
+		Log.d(this.getClass().getSimpleName(), "bound:" + bindSuccessful);
+
+		
+		if (parseService != null) {
+			// We do this here to handle the situation that we're "resuming" after being
+			// hidden. This ensures the Parse Server is "woken up".
+			registerWithParseServer();
+		}
+		
+		Log.d(this.getClass().getSimpleName(), "Exited onStart()");		
+	}
+
+
+	@Override
+	protected void onStop() {
+		Log.d(this.getClass().getSimpleName(), "Entered onStop()");		
+		super.onStop();
+
+		try {
+			parseService.send(Message.obtain(null, HandbagParseService.MSG_UI_SHUTDOWN_REQUEST));
+		} catch (RemoteException e) {
+			// Service crashed so just ignore it
+		}
+		
+/*
+        // For some reason this causes a "Service not registered" error:
+         * 
+		// Unbind from Parse Service if we're still connected.
+		if (parseServiceIsBound) {
+			unbindService(connParseService);
+			parseServiceIsBound = false;
+		}
+*/    
+
+		Log.d(this.getClass().getSimpleName(), "Exited onStop()");
+	}
+    
 }
