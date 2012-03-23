acd42dac43b23374650d3b49e352c0b834fbefc4
follower <follower@rancidbacon.com>
2012-03-24 08:43:46 +1300

    Add a test server to server up a test packet when a connection is received.

---
+++ dev-utils/test_handbag_packet_server.py
@@ -0,0 +1,40 @@
+# Used during development to serve up Handbag-format packets over TCP/IP
+# before the app or library are expanded to do so.
+
+import SocketServer
+
+import socket
+
+from generate_handbag_packet import createPacket
+
+class PacketServerHandler(SocketServer.BaseRequestHandler):
+    """
+    """
+    
+    def handle(self):
+        """
+        """
+        print "Client: %s" % str((self.request.getpeername()))
+
+        data = ["widget", "label", 20, 1, "My Label;\nHere, forever."]
+
+        return self.request.sendall(createPacket(data))
+
+if __name__ == "__main__":
+
+    # Note: This method of getting the local IP address may not work
+    #       in all situations.
+    HOST, PORT = socket.gethostbyname(socket.gethostname()), 0xba9
+
+    print "Host: %s Port: 0x%x" % (HOST, PORT)
+
+    # Create the server, binding to localhost on port 2985 (0xba9)
+    server = SocketServer.TCPServer((HOST, PORT), PacketServerHandler)
+
+    # Avoid "socket.error: [Errno 48] Address already in use" errors.
+    server.socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+    server.serve_forever()
+
+
+    
