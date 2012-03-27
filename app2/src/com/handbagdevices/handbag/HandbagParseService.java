72f98aba49a9c846f2ebb82d79f83c50ffae6065
follower <follower@rancidbacon.com>
2012-03-28 10:55:31 +1300

    Connect from the parse service to the WiFi comms service.

--- app2/src/com/handbagdevices/handbag/HandbagParseService.java
+++ app2/src/com/handbagdevices/handbag/HandbagParseService.java
@@ -1,7 +1,10 @@
 package com.handbagdevices.handbag;
 
 import android.app.Service;
+import android.content.ComponentName;
+import android.content.Context;
 import android.content.Intent;
+import android.content.ServiceConnection;
 import android.os.Handler;
 import android.os.IBinder;
 import android.os.Message;
@@ -23,7 +26,11 @@ public class HandbagParseService extends Service {
 	// Used to communicate with the UI Activity & Communication Service 
 	// We passively receive this connection.
 	Messenger uiActivity = null;
-	Messenger commService = null; // TODO: Implement service.	
+
+	// We pro-actively make this connection.
+	Messenger commsService = null;
+	boolean commsServiceIsBound = false;
+
 	
 	// Flag that should be checked
 	private boolean shutdownRequested = false;
@@ -43,7 +50,21 @@ public class HandbagParseService extends Service {
 					uiActivity = null;
 				}
 
-				testMessageHandler.sendEmptyMessageDelayed(0, 10000);				
+				
+				if (commsService != null) {
+					try {
+						commsService.send(Message.obtain(null, HandbagWiFiCommsService.MSG_UI_TEST_NETWORK));
+					} catch (RemoteException e) {
+						Log.d(this.getClass().getSimpleName(), "RemoteException on network test.");
+					}
+				}
+				
+				
+				// Only doing this once now...
+				//testMessageHandler.sendEmptyMessageDelayed(0, 10000);
+				
+				
+				
 			}
 	
 		}
@@ -54,6 +75,59 @@ public class HandbagParseService extends Service {
 		testMessageHandler.removeCallbacksAndMessages(null);
 		testMessageHandler.sendEmptyMessageDelayed(0, 5000);
 	}
+	
+	private void bindCommsService() {
+
+		// TODO: Use the chosen Comms Service (WiFi, USB ADK, BT?) 
+		boolean bindSuccessful = bindService(new Intent(HandbagParseService.this, HandbagWiFiCommsService.class), connCommsService, Context.BIND_AUTO_CREATE);
+		Log.d(this.getClass().getSimpleName(), "Comms Service bound:" + bindSuccessful);
+		
+		if (commsService != null) {
+			// We do this here to handle the situation that we're "resuming" after being
+			// hidden. This ensures the Comms Server is "woken up".
+			registerWithWiFiCommsService(); // TODO: Make generic.
+		}
+		
+	}
+	
+	// TODO: Just receive the required comms communication objects directly from the UI activity?
+	private ServiceConnection connCommsService = new ServiceConnection() {
+
+		public void onServiceConnected(ComponentName className, IBinder service) {
+			// Store the object we will use to communicate with the service.
+			commsService = new Messenger(service);
+			commsServiceIsBound = true;
+			Log.d(this.getClass().getSimpleName(), "Comms Service bound");
+			
+			registerWithWiFiCommsService();			
+		}
+
+		public void onServiceDisconnected(ComponentName className) {
+			// Called when the service crashes/unexpectedly disconnects.
+			commsService = null;
+			commsServiceIsBound = false;
+		}
+		
+	};
+	
+	
+	
+    private void registerWithWiFiCommsService() {
+    	
+    	Log.d(this.getClass().getSimpleName(), "Registering with WiFi Comms Service.");
+    	
+		Message msg = Message.obtain(null, MSG_PARSE_SERVICE_REGISTER); // TODO: Use Comms-specific constant or move here.
+		msg.replyTo = ourMessenger;
+		
+		try {
+			commsService.send(msg);
+		} catch (RemoteException ex1) {
+			// Service crashed so just ignore it
+			// TODO: Do something else?
+		}
+    	
+    }
+	
 	
 	// Used to receive messages from client(s)
 	public class IncomingParseServiceHandler extends Handler {
@@ -80,10 +154,42 @@ public class HandbagParseService extends Service {
 					}
 					
 					if (uiActivity != null) {
+						
+						bindCommsService();
+						
+						if (commsService != null) {
+							registerWithWiFiCommsService();
+						}
+						
 						startTestMessages();
 					}
 					
 					break;
+
+				case HandbagWiFiCommsService.MSG_PARSE_SERVICE_REGISTERED:
+					Log.d(this.getClass().getSimpleName(), "received: MSG_PARSE_SERVICE_REGISTERED");
+					break;
+					
+					
+					
+//				case MSG_COMMS_SERVICE_REGISTER:
+//					// TODO: Handle receiving this more than once?
+//					Log.d(this.getClass().getSimpleName(), "    MSG_COMM_SERVICE_REGISTER");
+//					commsService = msg.replyTo;
+//					
+//					try {
+//						commsService.send(Message.obtain(null, HandbagUI.MSG_UI_ACTIVITY_REGISTERED));
+//					} catch (RemoteException e) {
+//						// UI Activity client is dead so no longer try to access it.
+//						commsService = null;
+//					}
+//					
+//					if (commsService != null) {
+//						//startTestNetwork();
+//					}
+//					
+//					break;
+					
 					
 				case MSG_UI_SHUTDOWN_REQUEST:
 					Log.d(this.getClass().getSimpleName(), "    MSG_UI_SHUTDOWN_REQUEST");
