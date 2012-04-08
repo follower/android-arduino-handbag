420338e960aeb24e8301a9b8039f19eacd392d2d
follower <follower@rancidbacon.com>
2012-04-09 05:33:01 +1200

    Remove trailing whitespace.

--- app2/src/com/handbagdevices/handbag/HandbagUI.java
+++ app2/src/com/handbagdevices/handbag/HandbagUI.java
@@ -23,19 +23,19 @@ import android.widget.EditText;
 import android.os.Handler;
 
 public class HandbagUI extends Activity {
-	
+
 	// Messages to UI activity (i.e. us)
 	static final int MSG_UI_ACTIVITY_REGISTERED = 1;
 	static final int MSG_UI_TEST_MESSAGE = 2;
 	static final int MSG_UI_TEST_STRING_MESSAGE = 3;
 	static final int MSG_UI_TEST_ARRAY_MESSAGE = 4;
-	
+
 	Messenger parseService = null;
 	boolean parseServiceIsBound = false;
-	
+
 	Messenger commsService = null;
 	boolean commsServiceIsBound = false;
-	
+
 	// Used to receive messages from service(s)
 	class IncomingUiHandler extends Handler {
 		@Override
@@ -43,17 +43,17 @@ public class HandbagUI extends Activity {
 			WidgetConfig newWidget;
 
 			// TODO: Only continue if a shutdown hasn't been ordered? (Can we stop this handler directly?)
-			
+
 			switch (msg.what) {
 				case MSG_UI_ACTIVITY_REGISTERED:
 					Log.d(this.getClass().getSimpleName(), "received: MSG_UI_ACTIVITY_REGISTERED");
 					break;
-					
+
 				case MSG_UI_TEST_MESSAGE:
 					new AlertDialog.Builder(HandbagUI.this).setMessage("Message received!").show();
-					
+
 					Log.d(this.getClass().getSimpleName(), "Tid (ui):" + android.os.Process.myTid());
-					
+
 //					if (commsService != null) {
 //						try {
 //							commsService.send(Message.obtain(null, HandbagWiFiCommsService.MSG_UI_TEST_NETWORK));
@@ -61,19 +61,19 @@ public class HandbagUI extends Activity {
 //							Log.d(this.getClass().getSimpleName(), "RemoteException on network test.");
 //						}
 //					}
-					
+
 					break;
 
 				case MSG_UI_TEST_STRING_MESSAGE:
 					new AlertDialog.Builder(HandbagUI.this).setMessage(msg.getData().getString(null)).show();
 					break;
-				
+
 				case MSG_UI_TEST_ARRAY_MESSAGE:
 					// TODO: Should really check it's a label widget.
 					newWidget = LabelWidget.fromArray(msg.getData().getStringArray(null));
 
 					ViewGroup mainstage = (ViewGroup) findViewById(R.id.mainstage);
-					
+
 					// If we can't find mainstage it's probably because the
 					// active view has changed since the request was made.
 					if (mainstage != null) {
@@ -87,8 +87,8 @@ public class HandbagUI extends Activity {
 			}
 		}
 	}
-	
-	// Services we connect to will use this to communicate with us.	
+
+	// Services we connect to will use this to communicate with us.
     final Messenger ourMessenger = new Messenger(new IncomingUiHandler());
     
 	
@@ -99,8 +99,8 @@ public class HandbagUI extends Activity {
 			parseService = new Messenger(service);
 			parseServiceIsBound = true;
 			Log.d(this.getClass().getSimpleName(), "Parse Service bound");
-			
-			registerWithParseServer();			
+
+			registerWithParseServer();
 		}
 
 		public void onServiceDisconnected(ComponentName className) {
@@ -108,9 +108,8 @@ public class HandbagUI extends Activity {
 			parseService = null;
 			parseServiceIsBound = false;
 		}
-		
+
 	};
-	
 
 	private ServiceConnection connCommsService = new ServiceConnection() {
 
@@ -119,8 +118,8 @@ public class HandbagUI extends Activity {
 			commsService = new Messenger(service);
 			commsServiceIsBound = true;
 			Log.d(this.getClass().getSimpleName(), "Comms Service bound");
-			
-			registerWithWiFiCommsService();			
+
+			registerWithWiFiCommsService();
 		}
 
 		public void onServiceDisconnected(ComponentName className) {
@@ -128,111 +127,111 @@ public class HandbagUI extends Activity {
 			commsService = null;
 			commsServiceIsBound = false;
 		}
-		
+
 	};
-	
-	
+
+
 	private SharedPreferences appPrefs;
-	
+
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
-        
+
         appPrefs = getSharedPreferences("handbag", MODE_PRIVATE); // Add MODE_MULTI_PROCESS ?
 
-        populateFromPrefs();        
-        
+        populateFromPrefs();
+
     }
 
-    
+
     private void populateFromPrefs() {
 
         // TODO: Find a simpler approach for all these prefs...
         String pref_network_host_name = appPrefs.getString("network_host_name", "");
-        
+
         if (!pref_network_host_name.isEmpty()) {
         	EditText widgetHostName = (EditText) findViewById(R.id.hostName);
         	widgetHostName.setText(pref_network_host_name);
         }
 
 		attachPrefsSaver("network_host_name", R.id.hostName);
-        
-        
+
+
         String pref_network_host_port = appPrefs.getString("network_host_port", "");
-        
+
         if (!pref_network_host_port.isEmpty()) {
         	EditText widgetHostPort = (EditText) findViewById(R.id.hostPort);
-        	widgetHostPort.setText(pref_network_host_port);        	
+        	widgetHostPort.setText(pref_network_host_port);
         }
-    	
+
 		// TODO: Set default if empty?
 		attachPrefsSaver("network_host_port", R.id.hostPort);
 
     }
 
-    
+
     private void registerWithParseServer() {
-    	
+
     	Log.d(this.getClass().getSimpleName(), "Registering with Parse Server.");
-    	
+
 		Message msg = Message.obtain(null, HandbagParseService.MSG_UI_ACTIVITY_REGISTER);
-		msg.replyTo = ourMessenger;					
-		
+		msg.replyTo = ourMessenger;
+
 		try {
 			parseService.send(msg);
 		} catch (RemoteException ex1) {
 			// Service crashed so just ignore it
 			// TODO: Do something else?
 		}
-    	
+
     }
 
-    
+
     private void registerWithWiFiCommsService() {
-    	
+
     	Log.d(this.getClass().getSimpleName(), "Registering with WiFi Comms Service.");
-    	
+
 		Message msg = Message.obtain(null, HandbagParseService.MSG_UI_ACTIVITY_REGISTER); // TODO: Use Comms-specific constant or move here.
 		msg.replyTo = ourMessenger;
-		
+
 		try {
 			commsService.send(msg);
 		} catch (RemoteException ex1) {
 			// Service crashed so just ignore it
 			// TODO: Do something else?
 		}
-    	
+
     }
-    
+
 
 	@Override
 	protected void onStart() {
-		Log.d(this.getClass().getSimpleName(), "Entered onStart()");		
+		Log.d(this.getClass().getSimpleName(), "Entered onStart()");
 		super.onStart();
-		
+
 		// Bind to Parse Service which receives configuration information from the data
 		// source, and to which we send event information. TODO: Improve class name?
 		boolean bindSuccessful = bindService(new Intent(HandbagUI.this, HandbagParseService.class), connParseService, Context.BIND_AUTO_CREATE);
 		Log.d(this.getClass().getSimpleName(), "Parse Service bound:" + bindSuccessful);
 
-		// TODO: Use the chosen Comms Service (WiFi, USB ADK, BT?) 
+		// TODO: Use the chosen Comms Service (WiFi, USB ADK, BT?)
 		bindSuccessful = bindService(new Intent(HandbagUI.this, HandbagWiFiCommsService.class), connCommsService, Context.BIND_AUTO_CREATE);
 		Log.d(this.getClass().getSimpleName(), "Comms Service bound:" + bindSuccessful);
-		
+
 		if (parseService != null) {
 			// We do this here to handle the situation that we're "resuming" after being
 			// hidden. This ensures the Parse Server is "woken up".
 			registerWithParseServer();
 		}
-		
+
 		if (commsService != null) {
 			// We do this here to handle the situation that we're "resuming" after being
 			// hidden. This ensures the Comms Server is "woken up".
 			registerWithWiFiCommsService(); // TODO: Make generic.
 		}
-		
+
 //		if (commsService != null) {
 //			try {
 //				commsService.send(Message.obtain(null, HandbagWiFiCommsService.MSG_UI_TEST_NETWORK));
@@ -240,9 +239,9 @@ public class HandbagUI extends Activity {
 //				Log.d(this.getClass().getSimpleName(), "RemoteException on network test.");
 //			}
 //		}
-			
-		
-		Log.d(this.getClass().getSimpleName(), "Exited onStart()");		
+
+
+		Log.d(this.getClass().getSimpleName(), "Exited onStart()");
 	}
 
 
@@ -254,8 +253,8 @@ public class HandbagUI extends Activity {
 
 	@Override
 	protected void onStop() {
-		Log.d(this.getClass().getSimpleName(), "Entered onStop()");		
-		
+		Log.d(this.getClass().getSimpleName(), "Entered onStop()");
+
 		super.onStop();
 
 		try {
@@ -263,7 +262,7 @@ public class HandbagUI extends Activity {
 		} catch (RemoteException e) {
 			// Service crashed so just ignore it
 		}
-		
+
 		try {
 			commsService.send(Message.obtain(null, HandbagParseService.MSG_UI_SHUTDOWN_REQUEST));
 		} catch (RemoteException e) {
@@ -272,36 +271,36 @@ public class HandbagUI extends Activity {
 
 /*
         // For some reason this causes a "Service not registered" error:
-         * 
+         *
 		// Unbind from Parse Service if we're still connected.
 		if (parseServiceIsBound) {
 			unbindService(connParseService);
 			parseServiceIsBound = false;
 		}
-*/    
+*/
 
 		Log.d(this.getClass().getSimpleName(), "Exited onStop()");
 	}
-    
+
 	private void attachPrefsSaver(final String prefsName, int id) {
 
 		EditText widget = (EditText) findViewById(id);
 		if (widget != null) {
 			widget.addTextChangedListener(new TextWatcher() {
-				
+
 				public void onTextChanged(CharSequence s, int start, int before, int count) {
 					// TODO: Only save valid values? (e.g. support a supplied validation function?)
 					//       And/or only save on "Done"/lose focus instead?
 					appPrefs.edit().putString(prefsName, s.toString()).commit();
 				}
-				
+
 				public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
 				public void afterTextChanged(Editable s) {}
 			});
 		}
 
 	}
-	
+
 	private boolean showingMainStage = false;
 
 	private void displayMainStage() {
@@ -310,7 +309,7 @@ public class HandbagUI extends Activity {
 			showingMainStage = true;
 		}
 	}
-	
+
 	private void hideMainStage() {
 		if (showingMainStage) {
 			setContentView(R.layout.main);
@@ -319,14 +318,14 @@ public class HandbagUI extends Activity {
 			// TODO: Stop comms activity?
 		}
 	}
-	
+
 	private boolean isOnline() {
 		NetworkInfo netInfo = ((ConnectivityManager) getSystemService(CONNECTIVITY_SERVICE)).getActiveNetworkInfo();
-		
+
 		return (netInfo != null) ? netInfo.isConnected() : false;
 	}
 
-	
+
 	public void onClick_buttonConnect(View theView) {
 
 		// TODO: Make this check proactive and disable the button and/or
@@ -335,9 +334,9 @@ public class HandbagUI extends Activity {
 			new AlertDialog.Builder(this).setMessage("No wireless connection available.").show();
 			return;
 		}
-		
+
 		displayMainStage();
-		
+
 		// TODO: Provide some sort of status feedback.
 		if (commsService != null) {
 			try {
@@ -354,7 +353,7 @@ public class HandbagUI extends Activity {
 	public void onBackPressed() {
 
 		hideMainStage();
-		
+
 		// TODO: Need to solve "leaked connection" errors before re-enabling this:
 		//super.onBackPressed();
 	}
