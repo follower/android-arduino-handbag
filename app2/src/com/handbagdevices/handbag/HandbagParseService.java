d30dfeff1159d4f8181b2c0b736e55f2bfb56b34
follower <follower@rancidbacon.com>
2012-04-11 15:30:34 +1200

    Remove trailing whitespace.

--- app2/src/com/handbagdevices/handbag/HandbagParseService.java
+++ app2/src/com/handbagdevices/handbag/HandbagParseService.java
@@ -15,17 +15,17 @@ import android.util.Log;
 // TODO: Rename this because its purpose has changed a bit?
 
 public class HandbagParseService extends Service {
-	
+
 	// Messages from UI activity
 	static final int MSG_UI_ACTIVITY_REGISTER = 101;
 	static final int MSG_UI_SHUTDOWN_REQUEST = 102;
-	
+
 	static final int MSG_UI_EVENT_OCCURRED = 100;
-	
+
 	// Messages to communications service
-	static final int MSG_PARSE_SERVICE_REGISTER = 200;	
-	
-	// Used to communicate with the UI Activity & Communication Service 
+	static final int MSG_PARSE_SERVICE_REGISTER = 200;
+
+	// Used to communicate with the UI Activity & Communication Service
 	// We passively receive this connection.
 	Messenger uiActivity = null;
 
@@ -33,10 +33,10 @@ public class HandbagParseService extends Service {
 	Messenger commsService = null;
 	boolean commsServiceIsBound = false;
 
-	
+
 	// Flag that should be checked
 	private boolean shutdownRequested = false;
-	
+
 	private Handler testMessageHandler = new Handler() {
 
 		@Override
@@ -52,7 +52,7 @@ public class HandbagParseService extends Service {
 					uiActivity = null;
 				}
 
-				
+
 				if (commsService != null) {
 					try {
 						commsService.send(Message.obtain(null, HandbagWiFiCommsService.MSG_UI_TEST_NETWORK));
@@ -60,38 +60,38 @@ public class HandbagParseService extends Service {
 						Log.d(this.getClass().getSimpleName(), "RemoteException on network test.");
 					}
 				}
-				
-				
+
+
 				// Only doing this once now...
 				//testMessageHandler.sendEmptyMessageDelayed(0, 10000);
-				
-				
-				
+
+
+
 			}
-	
+
 		}
-	
+
 	};
-	
+
 	private void startTestMessages() {
 		testMessageHandler.removeCallbacksAndMessages(null);
 		testMessageHandler.sendEmptyMessageDelayed(0, 5000);
 	}
-	
+
 	private void bindCommsService() {
 
-		// TODO: Use the chosen Comms Service (WiFi, USB ADK, BT?) 
+		// TODO: Use the chosen Comms Service (WiFi, USB ADK, BT?)
 		boolean bindSuccessful = bindService(new Intent(HandbagParseService.this, HandbagWiFiCommsService.class), connCommsService, Context.BIND_AUTO_CREATE);
 		Log.d(this.getClass().getSimpleName(), "Comms Service bound:" + bindSuccessful);
-		
+
 		if (commsService != null) {
 			// We do this here to handle the situation that we're "resuming" after being
 			// hidden. This ensures the Comms Server is "woken up".
 			registerWithWiFiCommsService(); // TODO: Make generic.
 		}
-		
+
 	}
-	
+
 	// TODO: Just receive the required comms communication objects directly from the UI activity?
 	private ServiceConnection connCommsService = new ServiceConnection() {
 
@@ -100,8 +100,8 @@ public class HandbagParseService extends Service {
 			commsService = new Messenger(service);
 			commsServiceIsBound = true;
 			Log.d(this.getClass().getSimpleName(), "Comms Service bound");
-			
-			registerWithWiFiCommsService();			
+
+			registerWithWiFiCommsService();
 		}
 
 		public void onServiceDisconnected(ComponentName className) {
@@ -109,28 +109,28 @@ public class HandbagParseService extends Service {
 			commsService = null;
 			commsServiceIsBound = false;
 		}
-		
+
 	};
-	
-	
-	
+
+
+
     private void registerWithWiFiCommsService() {
-    	
+
     	Log.d(this.getClass().getSimpleName(), "Registering with WiFi Comms Service.");
-    	
+
 		Message msg = Message.obtain(null, MSG_PARSE_SERVICE_REGISTER); // TODO: Use Comms-specific constant or move here.
 		msg.replyTo = ourMessenger;
-		
+
 		try {
 			commsService.send(msg);
 		} catch (RemoteException ex1) {
 			// Service crashed so just ignore it
 			// TODO: Do something else?
 		}
-    	
+
     }
-	
-	
+
+
 	// Used to receive messages from client(s)
 	public class IncomingParseServiceHandler extends Handler {
 
@@ -139,33 +139,33 @@ public class HandbagParseService extends Service {
 			Log.d(this.getClass().getSimpleName(), "Tid (parse):" + android.os.Process.myTid());
 
 			Log.d(this.getClass().getSimpleName(), "Parse Service received message:");
-			
+
 			switch (msg.what) {
 				case MSG_UI_ACTIVITY_REGISTER:
 					// TODO: Handle receiving this more than once?
 					Log.d(this.getClass().getSimpleName(), "    MSG_UI_ACTIVITY_REGISTER");
 					uiActivity = msg.replyTo;
-					
+
 					shutdownRequested = false;
-					
+
 					try {
 						uiActivity.send(Message.obtain(null, HandbagUI.MSG_UI_ACTIVITY_REGISTERED));
 					} catch (RemoteException e) {
 						// UI Activity client is dead so no longer try to access it.
 						uiActivity = null;
 					}
-					
+
 					if (uiActivity != null) {
-						
+
 						bindCommsService();
-						
+
 						if (commsService != null) {
 							registerWithWiFiCommsService();
 						}
-						
+
 						//startTestMessages();
 					}
-					
+
 					break;
 
 				case HandbagWiFiCommsService.MSG_PARSE_SERVICE_REGISTERED:
@@ -178,40 +178,40 @@ public class HandbagParseService extends Service {
 //					// TODO: Handle receiving this more than once?
 //					Log.d(this.getClass().getSimpleName(), "    MSG_COMM_SERVICE_REGISTER");
 //					commsService = msg.replyTo;
-//					
+//
 //					try {
 //						commsService.send(Message.obtain(null, HandbagUI.MSG_UI_ACTIVITY_REGISTERED));
 //					} catch (RemoteException e) {
 //						// UI Activity client is dead so no longer try to access it.
 //						commsService = null;
 //					}
-//					
+//
 //					if (commsService != null) {
 //						//startTestNetwork();
 //					}
-//					
+//
 //					break;
-					
-					
+
+
 				case MSG_UI_SHUTDOWN_REQUEST:
 					Log.d(this.getClass().getSimpleName(), "    MSG_UI_SHUTDOWN_REQUEST");
-					
+
 					// Set a flag so repeating sub-tasks will stop themselves.
 					shutdownRequested = true;
 					// TODO: Be more proactive and stop them here instead?
 					break;
-				
+
 				default:
-					Log.d(this.getClass().getSimpleName(), "    (unknown)");					
-					super.handleMessage(msg);					
+					Log.d(this.getClass().getSimpleName(), "    (unknown)");
+					super.handleMessage(msg);
 			}
 		}
-		
+
 	}
 
 	// Clients will use this to communicate with us.
     final Messenger ourMessenger = new Messenger(new IncomingParseServiceHandler());
-	
+
 	@Override
 	public IBinder onBind(Intent intent) {
 		// Return our `messenger` instance to enable clients to send us messages.
