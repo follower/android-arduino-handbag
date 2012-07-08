4eb85c853688411aea5d76ebf0791e37b606b572
follower <follower@rancidbacon.com>
2012-07-09 02:27:27 +1200

    Extract implementation of main class into separate file.

---
+++ library/Handbag2/NetworkHandbag.cpp
@@ -0,0 +1,78 @@
+#include "NetworkHandbag.h"
+
+NetworkHandbag::NetworkHandbag(EthernetServer& server) : server(server) {
+  /*
+   */
+  // Ideally we'd use 'Server' instead but it doesn't have a 'available()' method
+  // we can use.
+  // Maybe we need to accept a 'Client' at a different stage...
+  // TODO: Handle all this better.
+
+  uiIsSetup = false;
+  client = EthernetClient();
+}
+
+int NetworkHandbag::begin(BASIC_CALLBACK(setupUICallback)) {
+  this->setupUICallback = setupUICallback;
+
+  server.begin();
+
+  return 1;
+}
+
+
+void NetworkHandbag::refresh() {
+  /*
+   */
+
+  if (!uiIsSetup) { // This implies we're not already connected to someone,
+    // whereas checking `client` just says if there's data
+    // available--it can be connected but still false.
+    client = server.available();
+  }
+
+  if (client) {
+
+    if (!uiIsSetup) {
+      Serial.println("setup ui");
+      // TODO: Initialise here
+
+      // TODO: Reset widget ids etc.
+      reset();
+
+      strm = &client;
+
+      if (setupUICallback != NULL) {
+	// NOTE: This *must* only be called after 'strm' is valid.
+	setupUICallback();
+      }
+
+      uiIsSetup = true;
+    }
+
+    // TODO: Simplify/improve the handling of disconnected/available issues.
+    if (client.connected()) {
+
+      processPacket();
+
+    } else {
+
+      Serial.println("client is being stopped. ");
+
+      client.stop(); // TODO: Unnecessary and/or enough?
+      uiIsSetup = false;
+    }
+  }
+}
+
+
+boolean NetworkHandbag::isConnected() {
+  /*
+   */
+  // Note: This kind of makes refresh() redundant and I'm
+  //       not convinced this is the best approach.
+  // TODO: Make better?
+  // TODO: Make sure all communication routines check for connection first?
+  refresh();
+  return uiIsSetup;
+}
--- library/Handbag2/NetworkHandbag.h
+++ library/Handbag2/NetworkHandbag.h
@@ -1,7 +1,10 @@
-#include "HandbagProtocolMixIn.h"
+#ifndef __NETWORK_HANDBAG_H__
+#define __NETWORK_HANDBAG_H__
 
 #include <SPI.h>
 #include <Ethernet.h>
+
+#include "HandbagProtocolMixIn.h"
 
 
 class NetworkHandbag : public HandbagProtocolMixIn {
@@ -14,84 +17,17 @@ private:
   boolean uiIsSetup;
 
 public:
-  NetworkHandbag(EthernetServer& server) : server(server) {
-    /*
-     */
-    // Ideally we'd use 'Server' instead but it doesn't have a 'available()' method
-    // we can use.
-    // Maybe we need to accept a 'Client' at a different stage...
-    // TODO: Handle all this better.
+  NetworkHandbag(EthernetServer& server);
 
-    uiIsSetup = false;
-    client = EthernetClient();
-  }
+  int begin(BASIC_CALLBACK(setupUICallback));
 
-  int begin(BASIC_CALLBACK(setupUICallback)) {
-    this->setupUICallback = setupUICallback;
+  void refresh();
 
-    server.begin();
-
-    return 1;
-  }
-
-
-  void refresh() {
-    /*
-     */
-
-    if (!uiIsSetup) { // This implies we're not already connected to someone,
-                      // whereas checking `client` just says if there's data
-                      // available--it can be connected but still false.
-      client = server.available();
-    }
-
-    if (client) {
-
-      if (!uiIsSetup) {
-        Serial.println("setup ui");
-        // TODO: Initialise here
-
-        // TODO: Reset widget ids etc.
-        reset();
-
-        strm = &client;
-
-        if (setupUICallback != NULL) {
-          // NOTE: This *must* only be called after 'strm' is valid.
-          setupUICallback();
-        }
-
-        uiIsSetup = true;
-      }
-
-      // TODO: Simplify/improve the handling of disconnected/available issues.
-      if (client.connected()) {
-
-        processPacket();
-
-      } else {
-
-	Serial.println("client is being stopped. ");
-
-        client.stop(); // TODO: Unnecessary and/or enough?
-        uiIsSetup = false;
-      }
-    }
-  }
-
-
-  boolean isConnected() {
-    /*
-     */
-    // Note: This kind of makes refresh() redundant and I'm
-    //       not convinced this is the best approach.
-    // TODO: Make better?
-    // TODO: Make sure all communication routines check for connection first?
-    refresh();
-    return uiIsSetup;
-  }
+  boolean isConnected();
 
 };
 
 // This enables us to have a single class name used publically
 #define HandbagApp NetworkHandbag
+
+#endif
