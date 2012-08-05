0cfe26b2cea6220a95ccbbdfb91cd6626ab1472c
follower <follower@rancidbacon.com>
2012-08-05 13:54:30 -0700

    Initial implementation (mostly copied from existing WiFi implementation) of service connection and main display activity start for USB connection. (Note that this doesn't currently work because (a) the associated service is only a skeleton; and, (b) the parse server is hard-coded to use the WiFi comms service.)

--- app2/src/com/handbagdevices/handbag/Activity_SetupUsb.java
+++ app2/src/com/handbagdevices/handbag/Activity_SetupUsb.java
@@ -1,10 +1,38 @@
 package com.handbagdevices.handbag;
 
 import android.app.Activity;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.ServiceConnection;
 import android.os.Bundle;
+import android.os.IBinder;
+import android.os.Messenger;
 import android.util.Log;
 
 public class Activity_SetupUsb extends Activity {
+
+    Messenger commsService = null;
+    boolean commsServiceIsBound = false;
+
+    private ServiceConnection connCommsService = new ServiceConnection() {
+
+        public void onServiceConnected(ComponentName className, IBinder service) {
+            // Store the object we will use to communicate with the service.
+            commsService = new Messenger(service);
+            commsServiceIsBound = true;
+            Log.d(this.getClass().getSimpleName(), "Comms Service bound");
+        }
+
+
+        public void onServiceDisconnected(ComponentName className) {
+            // Called when the service crashes/unexpectedly disconnects.
+            commsService = null;
+            commsServiceIsBound = false;
+        }
+
+    };
+
 
     // TODO: Leave all this for the comms service instead?
     private UsbAccessoryHandlerInterface usbHandler = null;
@@ -33,6 +61,16 @@ public class Activity_SetupUsb extends Activity {
     }
 
 
+    private void startDisplayActivity() {
+        // open display activity
+        Intent startDisplayActivityIntent = new Intent(Activity_SetupUsb.this, Activity_MainDisplay.class);
+
+        startDisplayActivityIntent.putExtra("COMMS_SERVICE", commsService); // TODO: Use a constant
+
+        startActivity(startDisplayActivityIntent);
+    }
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -47,4 +85,41 @@ public class Activity_SetupUsb extends Activity {
 
 
     }
+
+
+    // TODO: Refactor duplication with other Activity_Setup* classes?
+
+    @Override
+    protected void onStart() {
+        Log.d(this.getClass().getSimpleName(), "Entered onStart()");
+        super.onStart();
+
+        startService(new Intent(Activity_SetupUsb.this, CommsService_Usb.class));
+        boolean bindSuccessful = bindService(new Intent(Activity_SetupUsb.this, CommsService_Usb.class), connCommsService, Context.BIND_AUTO_CREATE);
+        Log.d(this.getClass().getSimpleName(), "Comms Service bound:" + bindSuccessful);
+
+        if (bindSuccessful) {
+            startDisplayActivity();
+        } else {
+            Log.e(this.getClass().getSimpleName(), "Comms service not bound--not starting display activity.");
+        }
+
+        Log.d(this.getClass().getSimpleName(), "Exited onStart()");
+    }
+
+
+    @Override
+    protected void onStop() {
+        Log.d(this.getClass().getSimpleName(), "Entered onStop()");
+
+        super.onStop();
+
+        // Note: MainDisplay activity is responsible for closing connections
+
+        unbindService(connCommsService);
+        // connCommsService = null;
+
+        Log.d(this.getClass().getSimpleName(), "Exited onStop()");
+    }
+
 }
