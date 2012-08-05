e6cea35daba14a4bed69cd9abdaf1ee18ef180d8
follower <follower@rancidbacon.com>
2012-08-05 16:11:38 -0700

    Handle registration of activity and parse services.

--- app2/src/com/handbagdevices/handbag/CommsService_Usb.java
+++ app2/src/com/handbagdevices/handbag/CommsService_Usb.java
@@ -6,9 +6,18 @@ import android.os.Handler;
 import android.os.IBinder;
 import android.os.Message;
 import android.os.Messenger;
+import android.os.RemoteException;
 import android.util.Log;
 
 public class CommsService_Usb extends Service {
+
+    // Used to communicate with the UI Activity & Communication Service
+    Messenger uiActivity = null; // Orders us around
+    Messenger parseService = null; // Receives our data, sends us its data.
+
+    // Flag that should be checked
+    private boolean shutdownRequested = false;
+
 
     // Used to receive messages from client(s)
     public class IncomingUsbCommsServiceHandler extends Handler {
@@ -19,6 +28,39 @@ public class CommsService_Usb extends Service {
             Log.d(this.getClass().getSimpleName(), "USB Comms Service received message:");
 
             switch (msg.what) {
+
+            // TODO: Don't actually get registered by UI activity because we don't use it (also in WiFi)?
+                case CommsService_WiFi.MSG_SETUP_ACTIVITY_REGISTER: // TODO: Move this constant into UI class?
+                    // TODO: Handle receiving this more than once?
+                    Log.d(this.getClass().getSimpleName(), "    MSG_SETUP_ACTIVITY_REGISTER");
+                    uiActivity = msg.replyTo;
+
+                    shutdownRequested = false;
+
+                    try {
+                        uiActivity.send(Message.obtain(null, Activity_MainDisplay.MSG_DISPLAY_ACTIVITY_REGISTERED)); // TODO: Change to specify who was registered with. ***
+                    } catch (RemoteException e) {
+                        // UI Activity client is dead so no longer try to access it.
+                        uiActivity = null;
+                    }
+
+                    break;
+
+
+                case HandbagParseService.MSG_PARSE_SERVICE_REGISTER: // TODO: Move this constant into UI class?
+                    // TODO: Handle receiving this more than once?
+                    Log.d(this.getClass().getSimpleName(), "    MSG_PARSE_SERVICE_REGISTER");
+                    parseService = msg.replyTo;
+
+                    try {
+                        parseService.send(Message.obtain(null, CommsService_WiFi.MSG_PARSE_SERVICE_REGISTERED));
+                    } catch (RemoteException e) {
+                        // Parse service client is dead so no longer try to access it.
+                        parseService = null;
+                    }
+
+                    break;
+
 
                 default:
                     Log.d(this.getClass().getSimpleName(), "    (unknown): " + msg.what);
