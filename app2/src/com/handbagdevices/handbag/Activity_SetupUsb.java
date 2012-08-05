c2b3de59ae4e05435d84f9d9dd00ceaef3cc8cd7
follower <follower@rancidbacon.com>
2012-08-05 12:33:01 -0700

    Quick add of initial work on USB setup--but most of this will be moved into the USB communications service instead.

--- app2/src/com/handbagdevices/handbag/Activity_SetupUsb.java
+++ app2/src/com/handbagdevices/handbag/Activity_SetupUsb.java
@@ -1,7 +1,50 @@
 package com.handbagdevices.handbag;
 
 import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
 
 public class Activity_SetupUsb extends Activity {
 
+    // TODO: Leave all this for the comms service instead?
+    private UsbAccessoryHandlerInterface usbHandler = null;
+
+
+    // TODO: Ideally this would be a static method in the interface...
+    private void setUsbHandler() {
+        try {
+            Class.forName("android.hardware.usb.UsbManager");
+            Log.d(this.getClass().getSimpleName(), "Using: 'android.hardware.usb.UsbManager'");
+            usbHandler = new UsbAccessoryHandlerHoneycomb();
+        } catch (ClassNotFoundException ex) {
+            try {
+                Class.forName("com.android.future.usb.UsbManager");
+                Log.d(this.getClass().getSimpleName(), "Using: 'com.android.future.usb.UsbManager'");
+                usbHandler = new UsbAccessoryHandlerGingerbread();
+            } catch (ClassNotFoundException ex1) {
+                Log.w(this.getClass().getSimpleName(), "Neither 'UsbManager' class found--no USB accessory support available.");
+            }
+        }
+    }
+
+
+    public Activity_SetupUsb() {
+        setUsbHandler();
+    }
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        if (usbHandler == null) {
+            // TODO: Display error message. (Shouldn't happen when only using intents.)
+            finish();
+            return;
+        }
+
+        usbHandler.setManager(this);
+
+
+    }
 }
