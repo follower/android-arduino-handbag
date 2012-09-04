c6a3213806b0b8c0dd9f0d97eac298031d728a60
follower <follower@rancidbacon.com>
2012-09-05 05:48:47 +1200

    Implement a hacky file extension-based work around to enable us to have both the Network and USB implementation in one library folder. (See header files for further explanation.) Prior to this change you needed to attempt to compile a USB demo sketch and a Network demo sketch before compilation would succeed. For me it's more important to have only a single library install no matter what Handbag communication protocol is used than to avoid this hacky work around.
    --HG--
    rename : library/Handbag2/Handbag.cpp => library/Handbag2/Handbag.cpp_
    rename : library/Handbag2/NetworkHandbag.cpp => library/Handbag2/NetworkHandbag.cpp_

--- library/Handbag2/NetworkHandbag.h
+++ library/Handbag2/NetworkHandbag.h
@@ -30,4 +30,14 @@ public:
 // This enables us to have a single class name used publically
 #define HandbagApp NetworkHandbag
 
+// The following is a hacky work around to enable us to have both
+// the Network and USB implementation in one library folder.
+// The Arduino IDE tries to compile all *.cpp files in the library
+// folder which will fail when the sketch is Network only (thus
+// doesn't include the USB headers) or USB only (thus doesn't
+// include the Network headers). If libraries could include
+// other libraries then we could probably work around this another
+// way but we can't, so we're stuck with this approach.
+#include "NetworkHandbag.cpp_"
+
 #endif
